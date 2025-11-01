4f5ae4ad7537b11b09f2a748bb6e00474b1a8075
nguyenthanhtam101 <tam43621@gmail.com>
2025-11-01 17:58:23 +0700

    Fix: Cap nhat duong dan file Hugging Face

--- app.py
+++ app.py
@@ -18,9 +18,12 @@ st.set_page_config( page_title="Hệ Thống Dự Đoán Đột Quỵ", page_ico
 
 # --- THAY ĐỔI: Định nghĩa tên repo HF và tên file ---
 HF_REPO_ID = "tam43621/stroke-prediction-models" 
-MODEL_A_FILENAME = "models/model_A_final.json"; SCALER_A_FILENAME = "models/scaler_A_final.pkl"
-COLS_A_FILENAME = "models/columns_A_final.pkl"; MODEL_B_FILENAME = "models/model_B_final.json"
-SCALER_B_FILENAME = "models/scaler_B_final.pkl"; COLS_B_FILENAME = "models/columns_B_final.pkl"
+MODEL_A_FILENAME = "models/model_A_final.json"
+SCALER_A_FILENAME = "models/scaler_A_final.pkl"
+COLS_A_FILENAME = "models/columns_A_final.pkl"
+MODEL_B_FILENAME = "models/model_B_final.json"
+SCALER_B_FILENAME = "models/scaler_B_final.pkl"
+COLS_B_FILENAME = "models/columns_B_final.pkl"
 X_TRAIN_SAMPLE_FILENAME = "models/X_train_sample_scaled.pkl"
 MODEL_C_FILENAME = "models/model2_C_resnet.h5" 
 
